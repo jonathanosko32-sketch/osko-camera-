85656bddef9731d92e188db958ec234bf7c10fee
jonathanosko32-sketch <jonathanosko32@gmail.com>
2026-07-24 04:45:25 -0700

    Refresh offline cache for universal scanner update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='osko-camera-v16';
+const CACHE_NAME='osko-camera-v17';
 const APP_SHELL=['./','./index.html','./styles.css','./barcode.css','./extras.css','./app.js','./barcode.js','./extras.js','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim()});
